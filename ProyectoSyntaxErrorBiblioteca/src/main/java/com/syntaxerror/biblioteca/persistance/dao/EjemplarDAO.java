7a2348083c8be2906feea582cb2c8b3f4127e553
azef4002 <fzapataarburua@gmail.com>
2025-05-02 20:07:31 -0500

    Revert "Fabian"
    This reverts commit 1c62e52755eb4202d24af31bd551e58bb1a37fbd.

--- ProyectoSyntaxErrorBiblioteca/src/main/java/com/syntaxerror/biblioteca/persistance/dao/EjemplarDAO.java
+++ ProyectoSyntaxErrorBiblioteca/src/main/java/com/syntaxerror/biblioteca/persistance/dao/EjemplarDAO.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import com.syntaxerror.biblioteca.model.EjemplarDTO;
 
 public interface EjemplarDAO {
-
     public Integer insertar(EjemplarDTO ejemplar);
 
     public EjemplarDTO obtenerPorId(Integer ejemplarId);
